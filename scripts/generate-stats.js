fd83e25cff46065958ee677f0824a7fedb266cb4
Uday-KiranK <uday.kottakota.k@gmail.com>
2026-03-24 04:15:17 +0530

    Update generate-stats.js

--- scripts/generate-stats.js
+++ scripts/generate-stats.js
@@ -3,21 +3,23 @@ import fs from "fs";
 const username = "Uday-KiranK";
 
 async function getStats() {
-  const res = await fetch(`https://api.github.com/users/${username}`);
-  const user = await res.json();
+  // Fetch repos
+  const reposRes = await fetch(`https://api.github.com/users/${username}/repos?per_page=100`);
+  const reposData = await reposRes.json();
 
-  // ✅ ALWAYS read template
+  const repoCount = reposData.length;
+
+  // Load template
   let svg = fs.readFileSync("svgs/stats-template.svg", "utf-8");
 
-  // 🔥 Replace values
+  // Replace values
   svg = svg.replaceAll("{{TOTAL_CONTRIBUTIONS}}", "N/A");
-  svg = svg.replaceAll("{{REPOS_CREATED}}", user.public_repos.toString());
+  svg = svg.replaceAll("{{REPOS_CREATED}}", repoCount.toString());
   svg = svg.replaceAll("{{CURRENT_STREAK}}", "N/A");
   svg = svg.replaceAll("{{COMMITS_THIS_YEAR}}", "N/A");
   svg = svg.replaceAll("{{LONGEST_STREAK}}", "N/A");
   svg = svg.replaceAll("{{PULL_REQUESTS}}", "N/A");
 
-  // ✅ WRITE to output file
   fs.writeFileSync("svgs/stats.svg", svg);
 }
 
