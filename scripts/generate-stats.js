a8dc3e2ca379f5e80daab2c6dd06148b60b58928
Uday-KiranK <uday.kottakota.k@gmail.com>
2026-03-24 04:22:30 +0530

    Update generate-stats.js

--- scripts/generate-stats.js
+++ scripts/generate-stats.js
@@ -3,37 +3,23 @@ import fs from "fs";
 const username = "Uday-KiranK";
 
 async function getStats() {
-// 🔥 fetch ALL repos (not just first page)
-let page = 1;
-let allRepos = [];
+  let page = 1;
+  let totalRepos = 0;
 
-while (true) {
-const res = await fetch(`https://api.github.com/users/${username}/repos?per_page=100&page=${page}`);
-const data = await res.json();
+  while (true) {
+    const res = await fetch(`https://api.github.com/users/${username}/repos?per_page=100&page=${page}`);
+    const data = await res.json();
 
-```
-if (data.length === 0) break;
+    if (!Array.isArray(data) || data.length === 0) break;
 
-allRepos = allRepos.concat(data);
-page++;
-```
+    totalRepos += data.length;
+    page++;
+  }
 
-}
+  let svg = fs.readFileSync("svgs/stats-template.svg", "utf-8");
+  svg = svg.replace("{{REPO_COUNT}}", totalRepos.toString());
 
-const repoCount = allRepos.length;
-
-// load template
-let svg = fs.readFileSync("svgs/stats-template.svg", "utf-8");
-
-// replace values
-svg = svg.replaceAll("{{TOTAL_CONTRIBUTIONS}}", "--");
-svg = svg.replaceAll("{{REPOS_CREATED}}", repoCount.toString());
-svg = svg.replaceAll("{{CURRENT_STREAK}}", "--");
-svg = svg.replaceAll("{{COMMITS_THIS_YEAR}}", "--");
-svg = svg.replaceAll("{{LONGEST_STREAK}}", "--");
-svg = svg.replaceAll("{{PULL_REQUESTS}}", "--");
-
-fs.writeFileSync("svgs/stats.svg", svg);
+  fs.writeFileSync("svgs/stats.svg", svg);
 }
 
 getStats();
